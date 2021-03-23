2194d36c68d88d70831fa3f318d35c6b98d97237
nataliea <natalie.akam@thetrainline.com>
2021-03-23 17:54:10 +0000

    Initial commit

---
+++ Player.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game
+{
+    public class Player
+    {
+        public string Name { get; set; }
+
+        public int Score { get; set; }
+
+    }
+}
