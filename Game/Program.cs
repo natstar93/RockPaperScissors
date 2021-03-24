0eba7c0ac2d7ca9a89891c8da88c8051bd716270
Michal Twarowski <michal.twarowski@thetrainline.com>
2021-03-24 15:47:17 +0000

    add folder for game

---
+++ Game/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome! What is your name?");
+            string name = Console.ReadLine();
+
+            Player player = new Player() { Name = name };
+
+            Console.WriteLine($"Hello {player.Name} your score is currently {player.Score}.");
+        }
+    }
+}
